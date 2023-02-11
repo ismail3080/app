aa56b6bb10bc7772abb3e65185758b17de733aee
ismail3080 <ismaillichir501@gmail.com>
2023-02-11 20:36:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useRef, useEffect, useState } from 'react';
+function App() {
+    const videoRef = useRef(null);
+    const photoRef = useRef(null);
+    const [hasPhoto, setHasPhoto] = useState(false);
+    const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({ 
+      video: { width: 1920, height: 1080}
+    })
+    .then(stream => {
+      let video = videoRef.current;
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  
+  }
+
+  const takePhoto = () => {
+    const width = 414;
+    const height = width / (16/9);
+    let video = videoRef.current;
+    let photo = photoRef.current;
+    photo.width = width;
+    photo.height = height;
+    let ctx = photo.getContext('2d');
+    ctx.drawImage(video, 0, 0, width, height);
+    setHasPhoto(true);
+  }
+
+  const closephoto = () => {
+    let photo = photoRef.current;
+    let ctx = photo.getContext('2d');
+    setHasPhoto(false);
+    ctx.clearRect(0, 0, photo.width, photo.height);
+  }
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+    
+    return (
+      <div className="App">
+        <div className="Camera">
+          <video ref={videoRef}></video>
+          <button onClick ={takePhoto}>SNAP!</button>
+          </div>
+          <div classsName={'result' + (hasPhoto ? 'hasPhoto':'') }>
+            <canvas ref={photoRef}></canvas>
+            <button onClick ={closephoto}>Close</button>
+          </div>
+      </div>
+    );
+
+    
+  }
+
+export default App;
